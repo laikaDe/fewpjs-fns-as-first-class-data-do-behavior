f8f15423d34ed32eeba5b9dc1a432df389d48085
Laika Deunert <laikadeunert@MacBook-Air.local>
2021-12-15 00:18:00 -0800

    commit tree df52200e71163124163a8e0d1403d324c069242d

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hourInString = time.split(":");
+  const hoursInInt = parseInt(hourInString,10);
+  if (hoursInInt < 12) {
+    return 'Good Morning';
+  }
+  else if (hoursInInt >=12 && hoursInInt <= 17){
+    return 'Good Afternoon';
+  }
+  else {
+  return 'Good Evening';
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  const element = document.getElementById("greeting");
+  element.innerText = message;
+  return message;
+}
